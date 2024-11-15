6628f25f1be633ee85256bd705c1fccabe74a32d
juanfe123 <111062004+juanfe123@users.noreply.github.com>
2024-11-15 17:41:49 -0500

    Add files via upload

---
+++ target/debug/cv0.d
@@ -0,0 +1,1 @@
+C:\Users\user\Desktop\paguina\ personal\cv0\target\debug\cv0.exe: C:\Users\user\Desktop\paguina\ personal\cv0\src\main.rs C:\Users\user\Desktop\paguina\ personal\cv0\templates\index.html
